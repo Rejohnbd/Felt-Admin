c689fb9bf757696fb50ee09d680d924e5cae1379
rejohnbd <rejohn.cse@gmail.com>
2023-04-20 23:09:10 +0600

    Optimize Device Type Code

--- pages/admin/setting/device-type/_id/edit.vue
+++ pages/admin/setting/device-type/_id/edit.vue
@@ -5,45 +5,16 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="header-title">Add Device Type</h4>
+                        <h4 class="header-title">Edit Device Type</h4>
                         <div class="row">
                             <div class="col-12">
                                 <div class="p-2">
-                                    <form @submit.prevent="handleUpdateSubmit">
-                                        <div class="form-group">
-                                            <label for="deviceTypeName">
-                                                Device Type Name
-                                                <span class="text-danger">*</span>
-                                            </label>
-                                            <input v-model="form.device_type_name" class="form-control" id="deviceTypeName"
-                                                :class="{ 'is-invalid': submitForm && $v.form.device_type_name.$error }"
-                                                type="text" required placeholder="Enter Device Type Name" />
-                                            <div v-if="submitForm && !$v.form.device_type_name.required"
-                                                class="invalid-feedback">Device Type Name is required.</div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="deviceTypeConfigureText">
-                                                Device Configure Setting
-                                                <span class="text-danger">*</span>
-                                            </label>
-                                            <div>
-                                                <textarea v-model="form.device_configure_text" id="deviceTypeConfigureText"
-                                                    class="form-control"
-                                                    :class="{ 'is-invalid': submitForm && $v.form.device_configure_text.$error }"
-                                                    placeholder="Enter Device Configure Setting" required></textarea>
-                                                <div v-if="submitForm && $v.form.device_configure_text.$error"
-                                                    class="invalid-feedback">
-                                                    <span v-if="!$v.form.device_configure_text.required">Device Configure
-                                                        Setting is Required.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group text-right m-b-0">
-                                            <button class="btn btn-primary" type="submit">Update</button>
-                                            <nuxt-link to="/admin/setting/device-type"
-                                                class="btn btn-secondary m-l-5 ml-1">Cancel</nuxt-link>
-                                        </div>
-                                    </form>
+                                    <DeviceTypeCreateEditForm 
+                                        v-on:mounted="initEdit"
+                                        ref="editDeviceTypeForm"
+                                        v-on:form-submitted="update"
+                                        operation="edit"
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -55,11 +26,14 @@
 </template>
 
 <script>
-import { required } from 'vuelidate/lib/validators';
+import DeviceTypeCreateEditForm from '@/components/admin/setting/device-type/DeviceTypeCreateEditForm.vue';
 
 export default {
     name: 'EditDeviceTypePage',
     middleware: ['auth', 'auth-admin'],
+    components: {
+        DeviceTypeCreateEditForm
+    },
     data() {
         return {
             title: 'Device Type Edit',
@@ -81,53 +55,34 @@ export default {
                     active: true,
                 },
             ],
-            form: {
-                device_type_name: '',
-                device_configure_text: '',
-            },
-            submitForm: false,
-        }
-    },
-    validations: {
-        form: {
-            device_type_name: {
-                required
-            },
-            device_configure_text: {
-                required
-            }
+            
         }
     },
     head: {
         titleTemplate: '%s Device Type Edit',
     },
-    created() {
-        if(this.$route.params.id) {
-            this.getDeviceTypeById(this.$route.params.id);
-        }else {
-            this.$router.push({
-                path: "/admin/setting/device-type",
-            });
-        }
-    },
     methods: {
-        // initEdit() {
-        //     console.log('here');
-        // }
-        async getDeviceTypeById(deviceTypeId) {
+        initEdit() {
+            this.$refs.editDeviceTypeForm.getDeviceTypeById();
+        },
+        async update(data) {
             this.$nuxt.$loading.start();
             try {
-                await this.$axios.get("admin/device-types/" + deviceTypeId)
+                await this.$axios.put(`admin/device-types/${data.id}`, data)
                     .then((response) => {
-                        if(response.status == 200) {
-                            this.form = Object.assign({}, response.data.data);
+                        if (response.status == 201) {
+                            this.$swal("Success!", response.data.message, "success");
+                            this.$router.push({
+                                path: "/admin/setting/device-type",
+                            });
                         }
                     }).catch((error) => {
                         if (error.response.status == 404) {
                             this.$toast.error(error.response.data.message);
-                            this.$router.push({
-                                path: "/admin/setting/device-type",
-                            });
+                        }
+
+                        if (error.response.status == 400) {
+                            this.$toast.error(error.response.data.message);
                         }
                         console.log(error.response.data);
                     });
@@ -136,43 +91,6 @@ export default {
                 console.log('Connection Error:', error);
             }
             this.$nuxt.$loading.finish();
-        },
-        async handleUpdateSubmit() {
-            this.submitForm = true;
-            this.$v.$touch();
-            if (!this.$v.$invalid) {
-                this.$nuxt.$loading.start();
-                try {
-                    await this.$axios.put(`admin/device-types/${this.form.id}`, this.form)
-                        .then((response) => {
-                            if (response.status == 201) {
-                                this.form.device_type_name = '';
-                                this.form.device_configure_text = '';
-                                this.submitForm = false;
-                                this.$swal("Success!", response.data.message, "success");
-                                this.$router.push({
-                                    path: "/admin/setting/device-type",
-                                });
-                            }
-                        }).catch((error) => {
-                            if (error.response.status == 404) {
-                                this.$toast.error(error.response.data.message);
-                            }
-
-                            if (error.response.status == 400) {
-                                this.$toast.error(error.response.data.message);
-                            }
-                            console.log(error.response.data);
-                        });
-                } catch (error) {
-                    this.submitForm = false;
-                    this.$toast.error("Connection Error");
-                    console.log('Connection Error:', error);
-                }
-                this.$nuxt.$loading.finish();
-            } else {
-                return;
-            }
         }
     }
 }
--- pages/admin/setting/device-type/add.vue
+++ pages/admin/setting/device-type/add.vue
@@ -9,47 +9,10 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="p-2">
-                                    <form @submit.prevent="handleSubmit">
-                                        <div class="form-group">
-                                            <label for="deviceTypeName">
-                                                Device Type Name
-                                                <span class="text-danger">*</span>
-                                            </label>
-                                            <input 
-                                                v-model="form.device_type_name"  
-                                                class="form-control" 
-                                                id="deviceTypeName"
-                                                :class="{ 'is-invalid': submitForm && $v.form.device_type_name.$error }" 
-                                                type="text" 
-                                                required
-                                                placeholder="Enter Device Type Name" 
-                                            />
-                                            <div v-if="submitForm && !$v.form.device_type_name.required" class="invalid-feedback">Device Type Name is required.</div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="deviceTypeConfigureText">
-                                                Device Configure Setting
-                                                <span class="text-danger">*</span>
-                                            </label>
-                                            <div>
-                                                <textarea
-                                                    v-model="form.device_configure_text"
-                                                    id="deviceTypeConfigureText"
-                                                    class="form-control" 
-                                                    :class="{ 'is-invalid': submitForm && $v.form.device_configure_text.$error }"
-                                                    placeholder="Enter Device Configure Setting"
-                                                    required
-                                                ></textarea>
-                                                <div v-if="submitForm && $v.form.device_configure_text.$error" class="invalid-feedback">
-                                                    <span v-if="!$v.form.device_configure_text.required">Device Configure Setting is Required.</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group text-right m-b-0">
-                                            <button class="btn btn-primary" type="submit">Submit</button>
-                                            <nuxt-link to="/admin/setting/device-type" class="btn btn-secondary m-l-5 ml-1">Cancel</nuxt-link>
-                                        </div>
-                                    </form>
+                                    <DeviceTypeCreateEditForm
+                                        v-on:form-submitted="create"
+                                        operation="create"
+                                    />
                                 </div>
                             </div>
                         </div>
@@ -61,11 +24,14 @@
 </template>
 
 <script>
-import { required } from 'vuelidate/lib/validators';
+import DeviceTypeCreateEditForm from '@/components/admin/setting/device-type/DeviceTypeCreateEditForm.vue';
 
 export default {
     name: 'AddDeviceTypePage',
     middleware: ['auth', 'auth-admin'],
+    components: {
+        DeviceTypeCreateEditForm
+    },
     data() {
         return {
             title: 'Device Type Add',
@@ -87,58 +53,34 @@ export default {
                     active: true,
                 },
             ],
-            form: {
-                device_type_name: '',
-                device_configure_text: '',
-            },
-            submitForm: false,
-        }
-    },
-    validations: {
-        form: {
-            device_type_name: {
-                required
-            },
-            device_configure_text: {
-                required
-            }
         }
     },
     head: {
         titleTemplate: '%s Device Type Add',
     },
     methods: {
-        async handleSubmit() {
-            this.submitForm = true;
-            this.$v.$touch();
-            if (!this.$v.$invalid) {
-                this.$nuxt.$loading.start();
-                try {
-                    await this.$axios.post('admin/device-types', this.form)
-                        .then((response) => {
-                            if(response.status == 201) {
-                                this.$swal("Success!", response.data.message, "success");
-                            }
-                            this.form.device_type_name = '';
-                            this.form.device_configure_text = '';
-                            this.submitForm = false;
-                        }).catch((error) => {
-                            if (error.response.status == 400) {
-                                this.$toast.error(error.response.data.message);
-                            } 
-                            console.log(error.response.data);
-                        });
-                } catch (error) {
-                    this.form.device_type_name = '';
-                    this.form.device_configure_text = '';
-                    this.submitForm = false;
-                    this.$toast.error("Connection Error");
-                    console.log('Connection Error:', error);
-                }
-                this.$nuxt.$loading.finish();
-            } else {
-                return;
+        async create(data) {
+            this.$nuxt.$loading.start();
+            try {
+                await this.$axios.post('admin/device-types', data)
+                    .then((response) => {
+                        if(response.status == 201) {
+                            this.$swal("Success!", response.data.message, "success");
+                            this.$router.push({
+                                path: "/admin/setting/device-type",
+                            });
+                        }
+                    }).catch((error) => {
+                        if (error.response.status == 400) {
+                            this.$toast.error(error.response.data.message);
+                        } 
+                        console.log(error.response.data);
+                    });
+            } catch (error) {
+                this.$toast.error("Connection Error");
+                console.log('Connection Error:', error);
             }
+            this.$nuxt.$loading.finish();
         }
     }
 }
